ef59a10acba092bdd3901f0913ecc923594363c5
jiminyun <jiminyun7@gmail.com>
2019-02-24 18:57:18 -0500

    more Controlers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -12,8 +12,18 @@ export const search = (req, res) => {
 
 export const video = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
-export const upload = (req, res) =>
+
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
