b55b7cff9573e835f4c4a372a16eb0552dce1825
jiminyun <jiminyun7@gmail.com>
2019-02-27 12:13:55 -0500

    styling

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -53,6 +53,7 @@ export const videoDetail = async (req, res) => {
 
   try {
     const video = await Video.findById(id);
+    //console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
